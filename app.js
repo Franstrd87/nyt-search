8db62e31b69df3275baf3ace1bf676fe1021ca20
Benjamin Candy <bcandyman93@gmail.com>
2019-10-19 14:06:19 -0700

    Added ability to retrieve search string from search-term id.

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 let APIKey = "3dg3PN4gT4BclNcWUT3UbG39o1XN5bAt" // enter API key 
 let queryURL1 = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="
-let queryURLSearch = "earthquake"
+let queryURLSearch //= //"earthquake"
 let queryURL2 = "&api-key=" + APIKey
 
-queryURL = queryURL1 + queryURLSearch + queryURL2
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response)
+  $("#search").on("click",function(){
+      event.preventDefault()
+      queryURLSearch = $("#search-term").val()
+      queryURL = queryURL1 + queryURLSearch + queryURL2
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response)
+    
+        //Loop through each item in object
+        for ( var i = 0; i < response.response.docs.length; i++){
+            console.log(response.response.docs[i].headline.main)
+    
+        }
+      });
 
-    //Loop through each item in object
-    for ( var i = 0; i < response.response.docs.length; i++){
-        console.log(response.response.docs[i].headline.main)
-
-    }
-  });
+      console.log($("#search-term").val())
+      console.log(this)
+  })
 
