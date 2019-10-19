df430f0b1015d32b911baec56403f7ad0a05322c
Benjamin Candy <bcandyman93@gmail.com>
2019-10-19 13:16:04 -0700

    Added api key and ajax command

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-let APIKey = "" // enter API key 
+let APIKey = "3dg3PN4gT4BclNcWUT3UbG39o1XN5bAt" // enter API key 
 let queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=" + APIKey
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+  
+  });
